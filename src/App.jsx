4bce1ed981da1396d4d672cd9204c4bcf7792430
Marc-Andre Baron-Campeau <m.baron.campeau@gmail.com>
2025-04-07 14:28:15 -0700

    adding relative path to iamge in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <h3>Made by Marc-André Baron-Campeau</h3>
       <h5>There is nothing else on this public page.</h5>
-      <img src="/SSDP3900-A02/bigWilly.png" />
+      <img src={`${import.meta.env.BASE_URL}/bigWilly.png`} />
       <h1>The Fresh Prince</h1>
     </>
   );
