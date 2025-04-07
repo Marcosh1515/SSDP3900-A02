0f3b083480b9035336b1edf75ecd4db5570a06d3
Marc-Andre Baron-Campeau <m.baron.campeau@gmail.com>
2025-04-07 12:24:44 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <img src="/marcFace.jpg" alt="" />
+      <p>
+        Edit <code>src/App.jsx</code> and save to test HMR
+      </p>
+    </>
+  );
+}
+
+export default App;
